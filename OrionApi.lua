2cde96d710e0c54f7706ed453126a8c5d0fda0b1
MertMocan <mertmocan0@gmail.com>
2024-08-27 21:24:39 +0300

    Add files via upload

---
+++ OrionApi.lua
@@ -0,0 +1,23 @@
+wait(3)
+print("🟢Thanks For Using Orion Executer!🟢")
+print("💫Orion Is Injected💫")
+print("🎗️discord.gg/WsjUcQ7Rbv🎗️")
+local StarterGui = game:GetService("StarterGui")
+StarterGui:SetCore("SendNotification", {
+	Title = "OrionAPI",
+	Text = "Thanks For Using Orion Executer!",
+	Icon = "rbxassetid://101005158739400",
+	Duration = 8
+})
+StarterGui:SetCore("SendNotification", {
+	Title = "OrionAPI",
+	Text = "Orion Is Injected",
+	Icon = "rbxassetid://101005158739400",
+	Duration = 8
+})
+StarterGui:SetCore("SendNotification", {
+	Title = "OrionAPI",
+	Text = "discord.gg/WsjUcQ7Rbv",
+	Icon = "rbxassetid://101005158739400",
+	Duration = 8
+})
